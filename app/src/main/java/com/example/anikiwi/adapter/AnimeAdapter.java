30c8993ef73e95c0ddc28d7a7681f16c04d88537
eloix2 <eloix2@gmail.com>
2023-06-26 02:55:53 +0200

    Updated adapter to admit other types of viewholder

--- app/src/main/java/com/example/anikiwi/adapter/AnimeAdapter.java
+++ app/src/main/java/com/example/anikiwi/adapter/AnimeAdapter.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -16,12 +17,12 @@ import com.bumptech.glide.request.RequestOptions;
 import com.example.anikiwi.R;
 import com.example.anikiwi.networking.Anime;
 
-import org.w3c.dom.Text;
-
 import java.util.List;
 
-public class AnimeAdapter extends RecyclerView.Adapter<AnimeAdapter.MyViewHolder> {
+public class AnimeAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     private Context context;
+    private final int VIEW_TYPE_ITEM = 0;
+    private final int VIEW_TYPE_LOADING = 1;
     private List<Anime> animes;
     private ItemClickListener clickListener;
     public AnimeAdapter(Context context, List<Anime> animes, ItemClickListener clickListener) {
@@ -34,16 +35,32 @@ public class AnimeAdapter extends RecyclerView.Adapter<AnimeAdapter.MyViewHolder
         this.animes = animes;
         notifyDataSetChanged();
     }
-    //initialize the contents of the View, adding the anime item layout to the RecyclerView
+    //initialize the contents of the View, adding the anime item layout to the RecyclerView, based on the viewType
     @NonNull
     @Override
-    public AnimeAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.rv_anime_item, parent, false);
-        return new MyViewHolder(view);
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view;
+        if(viewType == VIEW_TYPE_ITEM) {
+            view = LayoutInflater.from(context).inflate(R.layout.rv_anime_item, parent, false);
+            return new ItemViewHolder(view);
+        } else {
+            view = LayoutInflater.from(context).inflate(R.layout.rv_anime_loading, parent, false);
+            return new LoadingViewHolder(view);
+        }
+
     }
     //updates the contents of the itemView to reflect the item at the given position
     @Override
-    public void onBindViewHolder(@NonNull AnimeAdapter.MyViewHolder holder, @SuppressLint("RecyclerView") int position) {
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, @SuppressLint("RecyclerView") int position) {
+        if(holder instanceof ItemViewHolder) {
+            setAnimeItem((ItemViewHolder) holder, position);
+        }
+        else if(holder instanceof LoadingViewHolder) {
+            showLoadingView((LoadingViewHolder) holder, position);
+        }
+    }
+
+    private void setAnimeItem(ItemViewHolder holder, int position) {
         holder.tv_anime_title.setText(this.animes.get(position).getTitle());
         holder.itemView.setOnClickListener(v -> clickListener.onAnimeClick(animes.get(position)));
         Glide.with(context)
@@ -51,25 +68,40 @@ public class AnimeAdapter extends RecyclerView.Adapter<AnimeAdapter.MyViewHolder
                 .apply(RequestOptions.centerCropTransform())
                 .into(holder.img_anime);
     }
+
+    private void showLoadingView(LoadingViewHolder holder, int position) {
+        //ProgressBar would be displayed
+    }
+
     //returns the total number of items in the data set held by the adapter
     @Override
     public int getItemCount() {
-        if(this.animes != null)
-            return this.animes.size();
+        return animes == null ? 0 : animes.size();
+    }
 
-        return 0;
+    public int getItemViewType(int position) {
+        return animes.get(position) == null ? VIEW_TYPE_LOADING : VIEW_TYPE_ITEM;
     }
 
     //provides a reference to the views for each data item
-    public class MyViewHolder extends RecyclerView.ViewHolder {
+    public class ItemViewHolder extends RecyclerView.ViewHolder {
         TextView tv_anime_title;
         ImageView img_anime;
-        public MyViewHolder(@NonNull View itemView) {
+        public ItemViewHolder(@NonNull View itemView) {
             super(itemView);
             tv_anime_title = (TextView) itemView.findViewById(R.id.tv_anime_title);
             img_anime = (ImageView) itemView.findViewById(R.id.img_anime);
         }
     }
+
+    public class LoadingViewHolder extends RecyclerView.ViewHolder {
+        ProgressBar progressBar;
+        public LoadingViewHolder(@NonNull View itemView) {
+            super(itemView);
+            progressBar = itemView.findViewById(R.id.progressBar);
+        }
+    }
+
     //interface for click listener
     public interface ItemClickListener {
         void onAnimeClick(Anime anime);
